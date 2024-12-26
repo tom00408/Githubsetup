6cb4c3ffc73b4bcaa774939c7ae333527d81e799
Tom Justus Tiedtke <tom.tiedtke>
2024-12-26 23:34:04 +0100

    Initial Commit

---
+++ Githubsetup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Githubsetup
+//
+//  Created by Tom Tiedtke on 26.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Githubsetup/GithubsetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubsetupApp.swift
+//  Githubsetup
+//
+//  Created by Tom Tiedtke on 26.12.24.
+//
+
+import SwiftUI
+
+@main
+struct GithubsetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
